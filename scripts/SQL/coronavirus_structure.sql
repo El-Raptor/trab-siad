4edb2d52ac65160b396116734da241b40a332092
Felipe Salles Lopes <felipe28101997@gmail.com>
2020-05-20 17:41:28 -0400

    Atualizado script coronavirus_structure.sql

--- scripts/SQL/coronavirus_structure.sql
+++ scripts/SQL/coronavirus_structure.sql
@@ -1,12 +1,6 @@
 CREATE SCHEMA coronavirus;
 
 
-CREATE TABLE coronavirus.clima (
-	temp_max integer NOT NULL,
-	temp_min integer NOT NULL,
-	preciptacao decimal NOT NULL
-);
-
 CREATE TABLE coronavirus.temperatura (
 	id serial,
 	temp_celsius int NOT NULL,
@@ -46,7 +40,6 @@ CREATE TABLE coronavirus.covid19_mundo (
 	casos int NOT NULL,
 	mortes int NOT NULL,
 	pais_cod char(3) NOT NULL,
-	pop int NOT NULL,
 	total_leitos int NOT NULL,
 	PRIMARY KEY (id),
 	CONSTRAINT tempo_fk FOREIGN KEY (data_reg) REFERENCES coronavirus.tempo(id),
@@ -55,17 +48,28 @@ CREATE TABLE coronavirus.covid19_mundo (
 );
 
 CREATE TABLE coronavirus.casos_por_cidade (
-    data_reg date NOT NULL,
-    cidade text NOT NULL,
+    id serial,
+    id_data int NOT NULL,
+    cidade int NOT NULL,
     pais char(3) NOT NULL,
     casos integer,
-    mortes integer
+    PRIMARY KEY (id),
+    CONSTRAINT cidade_casos_cidade_fk FOREIGN KEY (cidade) REFERENCES coronavirus.cidade(id),
+    CONSTRAINT pais_casos_cidade_fk FOREIGN KEY (pais) REFERENCES coronavirus.pais(codigo_iso),
+    CONSTRAINT data_id_casos_cidade_fk FOREIGN KEY (id_data) REFERENCES coronavirus.tempo(id)
 );
 
 CREATE TABLE coronavirus.temperatura_cidade (
-    data_reg date NOT NULL,
-    temp_max integer,
-    temp_min integer,
-    cidade text NOT NULL,
-    pais char(3) NOT NULL
+    id serial,
+    id_data int NOT NULL,
+    temp_max integer NOT NULL,
+    temp_min integer NOT NULL,
+    cidade int NOT NULL,
+    pais char(3) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT cidade_temp_cidade_fk FOREIGN KEY (cidade) REFERENCES coronavirus.cidade(id),
+    CONSTRAINT pais_temp_cidade_fk FOREIGN KEY (pais) REFERENCES coronavirus.pais(codigo_iso),
+    CONSTRAINT data_id_temp_cidade_fk FOREIGN KEY (id_data) REFERENCES coronavirus.tempo(id),
+    CONSTRAINT temp_max_fk FOREIGN KEY (temp_max) REFERENCES coronavirus.temperatura(id),
+    CONSTRAINT temp_min_fk FOREIGN KEY (temp_min) REFERENCES coronavirus.temperatura(id)
 );
